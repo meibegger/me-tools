c925e6161e408df2d9b34a32d6dcccc808843085
Mandana Eibegger <me@schoener.at>
2016-03-11 13:17:27 +0100

    added tests

--- examples/examples.js
+++ examples/examples.js
@@ -1,44 +1,128 @@
-;(function(root, factory) {
+;
+(function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['meTools'], factory);
   } else {
     factory(meTools);
   }
-} (this, function(meTools) {
+}(this, function (meTools) {
 
-
-  console.log("meTools",meTools);
-
-var
-  val = {
-    a: "lorem ipsum",
-    b: 123,
-    c: [1,2,3],
-    d: document.getElementById('foo'),
-    e: {
-      e1: "dolor sit amet",
-      e2: 456,
-      e3: [4,5,6],
-      e4: document.getElementById('foo'),
-      e5: {
-        e51: 'foo'
+  // copyValues
+  var
+    val = {
+      a: "lorem ipsum",
+      b: 123,
+      c: [1, 2, 3],
+      d: document.getElementById('headline'),
+      e: {
+        e1: "dolor sit amet",
+        e2: 456,
+        e3: [4, 5, 6],
+        e4: document.getElementById('headline'),
+        e5: {
+          e51: 'foo'
+        }
       }
+    },
+    copy = meTools.copyValues(val);
+  console.log("--- COPIED");
+  console.log("original value", val);
+  console.log("copy", copy);
+
+  // manipulate copy
+  var newElement = "NEW",
+  manipulate = function (obj) {
+    if (Array.isArray(obj)) {
+      for (var i in obj) {
+        manipulate(obj[i]);
+      }
+      obj.push(newElement);
+
+    } else if (obj && typeof(obj) === 'object' && typeof(obj.tagName) === 'undefined') {
+      for (var key in obj) {
+        manipulate(obj[key]);
+      }
+      obj.new = newElement;
     }
-  },
-  copy = meTools.copyValues(val);
+  };
+  manipulate(copy);
+  console.log("--- MANIPULATED COPY");
+  console.log("original value", val);
+  console.log("copy", copy);
 
-copy.new = 'bar';
-console.log("COPIED!", val, copy);
+  // mergeObjects
+  var val2 = {
+    a: "Lorem Ipsum",
+    t: {
+      t1: [7, 8, 9],
+      t2: document.getElementById('headline')
+    }
+  };
+  console.log("-- MERGED");
+  console.log("val 1",val);
+  console.log("val 2",val2);
+  console.log("merged",meTools.mergeObjects(val, val2));
 
-var val2 = {
-  a: "Lorem Ipsum",
-  t: {
-    t1: [7,8,9],
-    t2: document.getElementById('foo')
-  }
-};
-console.log("MERGED",val,val2,meTools.mergeObjects(val,val2));
+  // isEmptyObject
+  console.log("--- IS EMPTY");
+  console.log(val,meTools.isEmptyObject(val));
+  console.log({},meTools.isEmptyObject({}));
 
-meTools.registerEvent(this,document.getElementById('foo'),'click',function() {alert('HALLO')});
+  // (un)registerEvent
+  var scope = {},
+    target1 = document.getElementById('target1'),
+    target2 = document.getElementById('target2'),
+    mouseDown = function () {
+      console.log("MOUSE DOWN");
+    },
+    mouseDown1 = function () {
+      console.log("MOUSE DOWN ON TARGET 1");
+    },
+    mouseDown2 = function () {
+      console.log("MOUSE DOWN ON TARGET 2");
+    },
+    mouseUp = function () {
+      console.log("MOUSE UP");
+    };
+  document.getElementById('registerEvents').addEventListener('click',function () {
+    meTools.unregisterEvent(scope); // reset
+    meTools.registerEvent(scope,target1,'mousedown',mouseDown);
+    meTools.registerEvent(scope,target1,'mousedown',mouseDown1);
+    meTools.registerEvent(scope,target1,'mouseup',mouseUp);
+    meTools.registerEvent(scope,target2,'mousedown',mouseDown);
+    meTools.registerEvent(scope,target2,'mousedown',mouseDown2);
+    meTools.registerEvent(scope,target2,'mouseup',mouseUp);
+  });
+  document.getElementById('unregisterAll').addEventListener('click',function () {
+    meTools.unregisterEvent(scope);
+  });
+  document.getElementById('unregisterTarget1').addEventListener('click',function () {
+    meTools.unregisterEvent(scope,target1);
+  });
+  document.getElementById('unregisterSpecific1').addEventListener('click',function () {
+    meTools.unregisterEvent(scope,null,null,mouseDown1);
+  });
+  document.getElementById('unregisterGeneral').addEventListener('click',function () {
+    meTools.unregisterEvent(scope,null,null,mouseDown);
+  });
+  document.getElementById('unregisterGeneral1').addEventListener('click',function () {
+    meTools.unregisterEvent(scope,target1,null,mouseDown);
+  });
+  document.getElementById('unregisterMouseDown').addEventListener('click',function () {
+    meTools.unregisterEvent(scope,null,'mousedown');
+  });
+
+  // throttle
+  window.addEventListener('mousemove',meTools.throttle(function() {
+    console.log("THROTTLED MOUSE MOVE")
+  }));
+
+  // debounce
+  window.addEventListener('mousemove',meTools.debounce(function() {
+    console.log("START MOUSE MOVE")
+  },0,true));
+  window.addEventListener('mousemove',meTools.debounce(function() {
+    console.log("END MOUSE MOVE")
+  }));
 
 }));
